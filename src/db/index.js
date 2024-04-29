eea4e1fb50b87498e73557d485f37bbc7f7a793c
rishuguptadj2001@gmail.com <rishuguptadj2001@gmail.com>
2024-04-29 17:53:55 +0530

    connect to db

---
+++ src/db/index.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDB = async () => {  //bd is on other continent
+  try {
+  const connectionInstance=  await mongoose.connect
+  (`${process.env.MONGODB_URI}/${DB_NAME}`)
+  console.log(`\n mongoDB connected !! DB HOST: ${connectionInstance.connection.host}`)
+//   console.log(connectionInstance);
+  } catch (error) {
+    console.log("MONGOOSE connection error", error);
+    process.exit(1);
+  }
+};
+export default connectDB;
+
